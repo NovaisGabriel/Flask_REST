f30fb21ed8afa67b9d2299d770d0f1742ebd2701
NovaisGabriel <gabrieel.novais@gmail.com>
2025-02-04 12:05:21 -0300

    better structure for flask projects

--- db.py
+++ db.py
@@ -1,11 +1,12 @@
+"""
+db.py
+---
+
+Later on, this file will be replaced by SQLAlchemy. For now, it mimics a database.
+Our data storage is:
+    - stores have a unique ID and a name
+    - items have a unique ID, a name, a price, and a store ID.
+"""
+
 stores = {}
-items = {
-    1: {
-        "name": "Chair",
-        "price": 15.99
-    },
-    2: {
-        "name": "Table",
-        "price": 180.50
-    }
-}
+items = {}
